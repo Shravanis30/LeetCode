a95ca4861407172c8b44b20dad755307a70e6ef7
Shravanis30 <salunkeshravani561@gmail.com>
2025-02-05 00:34:03 +0530

    Day 1 of LeetCode

--- D1P1.cpp
+++ D1P1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 vector<int> twoSum(vector<int> &arr, int target)
 {
     unordered_map<int, int> mp;
-    for (int i = 0; i < arr.size(); i++)
+    for (int i = 0; i < arr.size(); i++)    
     {
         int complement = target - arr[i];
         if (mp.find(complement) != mp.end())
